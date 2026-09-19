957f5ed9b3ec63b92fbb725fcbeaa062706f91a1
knightrishi <arnav.ara05@gmail.com>
2026-09-19 20:29:02 +0530

    ReAct = Reasoning + Acting learned

--- week2/day2/reAct.py
+++ week2/day2/reAct.py
@@ -12,7 +12,7 @@ if not my_api_key:
     raise ValueError("API key kaha hai bhai")
 
 client=Groq(api_key=my_api_key)
-model="llama-3.3-70b-versatile"
+model="openai/gpt-oss-120b"
 
 def get_product_price(product):
     if product == 'iPhone 17':
